83bcf152fe1560ac9d921f05dfd14d2f1b05323e
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2024-05-27 19:49:11 +0300

    sphere test and getNormal

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,7 +2,10 @@ package geometries;
 import Primitives.Point;
 import Primitives.Vector;
 
+/** sphere class is a polygon represented by a point and a radius*/
 public class Sphere {
+
+    /** A point that represents the center of the sphere*/
     final Point _center ;
     final double radius;
     /**
@@ -14,5 +17,20 @@ public class Sphere {
         _center = center;
         this.radius = radius;
     }
+    /**
+     * Returns the center point of the sphere.
+     *
+     * @return The center point of the sphere.
+     */
+    public Point getCenter() {
+        return _center;
+    }
 
+    public Vector getNormal(Point p0) {
+        return p0.subtract(_center).normalize();    }
+
+    @Override
+    public String toString() {
+        return "Sphere{" + "\ncenter=" + _center + "\nradius=" + radius + "\n}";
+    }
 }
