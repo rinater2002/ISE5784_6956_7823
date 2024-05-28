5cc7acc7c797fcad4c57c7d2272d5d96a11ef1e1
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2024-05-28 13:11:07 +0300

    add func

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -15,6 +15,11 @@ public class Triangle extends Polygon{
     Triangle(Point p1, Point p2, Point p3){
         super(p1, p2, p3);
     }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return plane.getNormal();
+    }
 }
 
 
